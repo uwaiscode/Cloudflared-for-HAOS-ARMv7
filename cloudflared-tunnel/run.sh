789bce50309bdc45092d51fe8b1fc5674a326ccb
Abu uwais <uwaiscode@gmail.com>
2026-04-29 10:53:05 +0000

    update run.sh

--- cloudflared-tunnel/run.sh
+++ cloudflared-tunnel/run.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
 
+# Load bashio library secara manual
+source /usr/lib/bashio/bashio.sh
+
+# Sekarang bashio sudah bisa digunakan
 TOKEN=$(bashio::config 'token')
 
 if [ -z "$TOKEN" ] || [ "$TOKEN" == "null" ]; then
@@ -11,4 +15,7 @@ if [ -z "$TOKEN" ] || [ "$TOKEN" == "null" ]; then
 fi
 
 bashio::log.info "Initializing Cloudflare Tunnel for ARMv7..."
+bashio::log.info "Connecting to Cloudflare Edge..."
+
+# Menjalankan cloudflared
 exec /usr/local/bin/cloudflared tunnel --no-autoupdate run --token "$TOKEN"
